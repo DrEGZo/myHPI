6863b034802488e19064c184917d9e685e431112
DrEGZo <konacom@web.de>
2022-05-16 17:26:05 +0200

    show menu items only when allowed to view page

--- myhpi/core/templatetags/core_extras.py
+++ myhpi/core/templatetags/core_extras.py
@@ -2,7 +2,8 @@ from django import template
 
 register = template.Library()
 
+# logic for this was moved to context.py
 
-@register.filter
-def menu_children(page):
-    return page.get_children().in_menu()
+#@register.filter
+#def menu_children(page):
+#    return page.get_children().in_menu().specific()
