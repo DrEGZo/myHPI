5b98911846b9ded4db0849365748e6109885bb43
DrEGZo <konacom@web.de>
2022-05-27 23:46:10 +0200

    removed core_extras since functionality was moved

--- myhpi/core/templatetags/core_extras.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-register = template.Library()
-
-# logic for this was moved to context.py
-
-# @register.filter
-# def menu_children(page):
-#    return page.get_children().in_menu().specific()
